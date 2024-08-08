16d2a68942d5c59f807b215a3d65f1c3ad790498
ankit-lang <rankit2883@gmail.com>
2024-08-08 16:58:53 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import ResponsiveAppBar from './components/Navbar'
+import { Button } from './components/ui/button'
+
+function App() {
+ 
+
+  return (
+    <>
+     {/* <Navbar/> */}
+     <Button>
+      Hwllo
+     </Button>
+     {/* <ResponsiveAppBar/> */}
+ 
+     
+    </>
+  )
+}
+
+export default App
